91f94d79ff6f54c84bc1242d9d0bd615b46bf875
SINEE PAUNGAM <spaungam@us.ibm.com>
2017-02-27 15:16:29 -0600

    add a fake junit test built with ant

---
+++ test/com/ibm/cloudoe/samples/HelloResourceTest.java
@@ -0,0 +1,16 @@
+package com.ibm.cloudoe.samples;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloResourceTest {
+
+	@Test
+	public void test() {
+		HelloResourceTest resource = new HelloResourceTest();
+		
+		assertTrue("Unexpected value", true);
+	}
+
+}
